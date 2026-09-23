4f91abcc713576f5d78c77d308e6cb55bd27e0d8
Hagen Mahnke <hagenmahnke@googlemail.com>
2026-09-23 15:10:59 +0200

    Add basic authentication, user management and top navigation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+if Rails.env.local?
+  [
+    { name: "Player One",  email_address: "player.one@test.de",  password: "playerone" },
+    { name: "Player Two",  email_address: "player.two@test.de",  password: "playertwo" },
+    { name: "Game Master", email_address: "game.master@test.de", password: "gamemaster" }
+  ].each do |attributes|
+    User.find_or_initialize_by(email_address: attributes[:email_address]).update!(attributes)
+  end
+end
