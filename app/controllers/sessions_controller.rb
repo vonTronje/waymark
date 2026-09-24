89392f4b78f4a1588c8bbf257309a98ceb7ffa1a
Hagen Mahnke <hagenmahnke@googlemail.com>
2026-09-24 11:25:26 +0200

    Return 303 see_other on redirects
    As expected by Turbo
    https://turbo.hotwired.dev/handbook/drive#redirecting-after-a-form-submission

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,9 +11,9 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate_by(params.permit(:email_address, :password))
       start_new_session_for user
-      redirect_to after_authentication_url
+      redirect_to after_authentication_url, status: :see_other
     else
-      redirect_to new_session_path, alert: "Try another email address or password."
+      redirect_to new_session_path, alert: "Try another email address or password.", status: :see_other
     end
   end
 
