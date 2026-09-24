87d570befdcea673a43fd46280ea713b56a1372b
Hagen Mahnke <hagenmahnke@googlemail.com>
2026-09-24 12:54:33 +0200

    Add ActionText using trix with Active Storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,9 @@ Rails.application.configure do
   # Highlight code that triggered redirect in logs.
   config.action_dispatch.verbose_redirect_logs = true
 
+  # Store uploaded files on the local file system (see config/storage.yml for options).
+  config.active_storage.service = :local
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,9 @@ Rails.application.configure do
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
 
+  # Store uploaded files on the local file system (see config/storage.yml for options).
+  config.active_storage.service = :local
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
