99261ea47fcf338c6a6b3a17213d37149352daa0
Hagen Mahnke <hagenmahnke@googlemail.com>
2026-09-24 11:27:20 +0200

    Remove rate_limit on session#create
    Not necessary at this point in development.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
   skip_after_action :verify_authorized
   skip_after_action :verify_policy_scoped
-  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_path, alert: "Try again later." }
 
   def new
     redirect_to after_authentication_url if authenticated?
